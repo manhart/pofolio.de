f8e227c7a1de06edcbbfaa44b12c465f01d08b18
Alexander Manhart <alexander@manhart-it.de>
2023-10-09 22:32:37 +0200

    Fixed decoding of authentication data

--- config/dbaccess.inc.php
+++ config/dbaccess.inc.php
@@ -2,7 +2,7 @@
 return [
     'mysql_auth' => [
         'localhost' => [
-            DB_POFOLIO => getenv('MARIADB_AUTH')
+            DB_POFOLIO => json_decode(getenv('MARIADB_AUTH'), true, 512, JSON_THROW_ON_ERROR)
         ]
     ]
 ];
