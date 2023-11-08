9953f890c5a757d23130a0b172a65fe6a8df2ff7
Alexander Manhart <alexander@manhart-it.de>
2023-11-08 20:44:20 +0100

    Refactor EtfList with the extraction of redundant methods
    To eliminate repetition, the EtfList was refactored by removing multiple getter functions (getSymbol, getExchange, getExchangeShortName, getPrice, getName, getType). This functionality has now been moved to the superclass StockList, allowing for easier maintenance and clear code readability. The EtfList now simply extends the StockList, utilizing the inherited methods to retrieve the same information.

--- classes/FMP/Response/EtfList.php
+++ classes/FMP/Response/EtfList.php
@@ -10,37 +10,7 @@
 
 namespace pofolio\classes\FMP\Response;
 
-class EtfList extends Response
+class EtfList extends StockList
 {
     protected static string $url = 'v3/etf/list';
-
-    public function getSymbol(): string
-    {
-        return $this->getResponseValueAsString('symbol');
-    }
-
-    public function getExchange(): string
-    {
-        return $this->getResponseValueAsString('exchange');
-    }
-
-    public function getExchangeShortName(): string
-    {
-        return $this->getResponseValueAsString('exchangeShortName');
-    }
-
-    public function getPrice(): float
-    {
-        return $this->getResponseValueAsFloat('price');
-    }
-
-    public function getName(): string
-    {
-        return $this->getResponseValueAsString('name');
-    }
-
-    public function getType(): string
-    {
-        return $this->getResponseValueAsString('type');
-    }
 }
